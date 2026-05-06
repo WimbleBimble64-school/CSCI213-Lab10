34908d9e808313fe1c3ca2620a83d322c6c8f53b
Joshua Bodde <joshuabodde@go.rmc.edu>
2026-05-06 14:02:50 -0400

    Update CourseInfoUpdated.java

--- CourseInfoUpdated.java
+++ CourseInfoUpdated.java
@@ -2,3 +2,10 @@
   For this section we are doing the same thing but adding classTime and number of students
   Author: Joshua Bodde
 */
+public CourseInfoUpdated{
+
+  public static void main(String[] args){
+
+
+    }
+}
