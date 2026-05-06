e16e09f327608889df07e2a59d2387c55f7fcdf4
Joshua Bodde <joshuabodde@go.rmc.edu>
2026-05-06 14:04:50 -0400

    Update CourseInfoUpdated.java

--- CourseInfoUpdated.java
+++ CourseInfoUpdated.java
@@ -6,10 +6,10 @@ public CourseInfoUpdated{
 
   public static void main(String[] args){
     System.out.println("Course Name: Object-Oriented Programming");
-		System.out.println("Instructor: Dr. Elouni");
-		System.out.println("Semester: Spring 2026");
-		System.out.println("Credits : 4");
-		System.out.println("Classroom: COP 204");
+	System.out.println("Instructor: Dr. Elouni");
+	System.out.println("Semester: Spring 2026");
+	System.out.println("Credits : 4");
+	System.out.println("Classroom: COP 204");
 
     //Updated Section
     System.out.println("Class Time: 1:10 - 2:40 pm");
